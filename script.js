31d1636b80b00dcc0c14f34ac6e3cfd20e08b599
zonayedpcav2 <zonayedpcadotcom@gmail.com>
2017-09-13 17:36:57 +0530

    finishing up

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ converter.prototype.numCon = function() {
 		normalResult: document.querySelector('.normal')
 	}
 
+	document.querySelector('.inNum').textContent = this.num;
+
 	if(this.num <= 1) {
 		var rem = this.num % this.divide;
 		this.num = Math.trunc(this.num / this.divide);
@@ -46,13 +48,17 @@ converter.prototype.numCon = function() {
 		console.log('Result: ' + this.num + ' and Remaindar: ' + rem);
 		getDom.normalResult.innerHTML += '<li class="num">'+ this.divide + '</li>';
 		getDom.normalResult.innerHTML += '<li class="normalRes">'+ this.num + '</li>';
-		//getDom.normalResult.innerHTML += '<li class="normalRem">'+ rem + '</li><br>';
+		
 		if(rem > 9) {
 			getDom.normalResult.innerHTML += '<li class="normalRem">'+ hexCon + '</li><br>';
+			document.querySelector('.stndrd-result').innerHTML += '<span class = "stdresult">' + hexCon + '</span>';
 		} else {
 			getDom.normalResult.innerHTML += '<li class="normalRem">'+ rem + '</li><br>';
-		}
+			document.querySelector('.stndrd-result').innerHTML += '<span class = "stdresult">' + rem + '</span>';	
+		} 
 	}
+
+	document.querySelector('.stndrd-result').innerHTML += '<span class = "stdresult">' + this.num + '</span>';
 
 	return {
 		retDom: function() {
@@ -64,6 +70,7 @@ converter.prototype.numCon = function() {
 var calculator = function(event) {
 	event.preventDefault();
 	document.querySelector('.normal').innerHTML = '';
+	document.querySelector('.stndrd-result').innerHTML = '';
 	document.querySelector('.normal').innerHTML += '<li class="num">'+ 'D' + '</li>';
 	document.querySelector('.normal').innerHTML += '<li class="normalRes">'+ 'R' + '</li>';
 	document.querySelector('.normal').innerHTML += '<li class="normalRem">'+ '%' + '</li><br>';
